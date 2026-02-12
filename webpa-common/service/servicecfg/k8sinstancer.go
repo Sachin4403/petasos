6982b5b2eda5c91b75c9c19111c878d26c99e93a
Sachin Narang <sachin.narang@sachin.narang>
2026-02-12 20:00:41 +0530

    using k8s hostname instead of IP

--- webpa-common/service/servicecfg/k8sinstancer.go
+++ webpa-common/service/servicecfg/k8sinstancer.go
@@ -153,7 +153,7 @@ func (i *instancer) extractInstances(endpoints *corev1.Endpoints) []string {
 		// Add all ready addresses
 		for _, addr := range subset.Addresses {
 			i.logger.Info("looping over the EndpointAddress", zap.Any("addr", addr))
-			instance := formatEndpointInstance(i.watch.scheme(), addr.Hostname, endpoints.Namespace, port)
+			instance := formatEndpointInstance(i.watch.scheme(), addr.Hostname, endpoints.Namespace, i.watch.Service, port)
 			instances = append(instances, instance)
 		}
 	}
@@ -183,8 +183,8 @@ func (i *instancer) findPort(ports []corev1.EndpointPort) int32 {
 }
 
 // formatEndpointInstance creates an instance string from endpoint data
-func formatEndpointInstance(scheme, address, namespace string, port int32) string {
-	return fmt.Sprintf("%s://%s.%s:%d", scheme, address, namespace, port)
+func formatEndpointInstance(scheme, address, namespace, serviceName string, port int32) string {
+	return fmt.Sprintf("%s://%s.%s.%s.svc.cluster.local:%d", scheme, address, serviceName, namespace, port)
 }
 
 // update notifies all registered channels of a new event
